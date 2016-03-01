99463e084816cc744f211544525107868c68c969
Israel Canul <israel.canul@gmail.com>
2016-03-01 17:39:08 -0500

    se implementa las pushnotifications

---
+++ routes/push.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var GCMPush = require('gcm-push');
+var gcm = new GCMPush('AIzaSyBw0dGqTqt45R9edV5zT4o-wGYbc5Rca9U'); 
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	gcm.notifyDevice('APA91bE1tx1W0EO38J64jrhQady1SWTpW68KgMKFstVZo_UInOWNBCvSJ2s04JooT7TCCgIocHlcT-1xDPSWkj35giQGlweZkpr1iyLVag577buhmsRyr3cfvhltkonNKd_5JR4R2DZb', 'notification title', 'my message');
+  	res.send('respond with a resource');
+});
+
+module.exports = router;
